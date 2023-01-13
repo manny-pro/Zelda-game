672130f94fe9a5062d83e5e57372fc405a8e9aa0
manny <115085160+manny-pro@users.noreply.github.com>
2023-01-13 07:53:56 +0100

    all

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Zelda-game
-game in Typescript 
+# INSTALLATION
+
+1 - "npm i" inside mods folder. <br />
+2 - "npm run dev" inside mods folder.
---
+++ mods/utils/interfaces.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+exports.leaderboard = void 0;
+var leaderboard_1 = require("./leaderboard");
+exports.leaderboard = leaderboard_1["default"];
+;
